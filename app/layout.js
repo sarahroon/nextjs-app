aeafba6cbdca9e74b475dc64ffdfe8e400527b9f
SARAH ROONEY <SARAHROONEY130500@GMAIL.COM>
2026-03-08 14:33:30 +0000

    Fix Bitcount Grid Double Ink font and update layout

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Load the font from Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double+Ink&display=swap"
           rel="stylesheet"
